c68d0db7d865110eeb944bacaef1609aede2255a
Susamaru <90692714+SilveonSenpai@users.noreply.github.com>
2025-12-19 15:53:14 +0200

    added imports to a bugged file

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,7 +1,8 @@
+import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/useAuth";
 
-export default function ProtectedRoute({ children }: { children: JSX.Element }) {
+export default function ProtectedRoute({ children }: { children: React.ReactElement }) {
   const { isAdmin } = useAuth();
   return isAdmin ? children : <Navigate to="/login" replace />;
 }
